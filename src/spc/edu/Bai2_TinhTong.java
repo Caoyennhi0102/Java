7ea638b0da30fda88de7fc9b286be154932139d7
Caoyennhi0102 <luuvinhphat1487@gmail.com>
2024-09-10 20:19:37 +0700

    JAVA_3

--- src/spc/edu/Bai2_TinhTong.java
+++ src/spc/edu/Bai2_TinhTong.java
@@ -11,6 +11,8 @@ package spc.edu;
 public class Bai2_TinhTong {
     public static void main(String[] args) {
         System.out.println("Chương Trình Tính Tổng Số Nguyên");
-        
+        int a = 1 , b = 8;
+        int Tong = a + b;
+        System.out.println("Tong = " + Tong);
     }
 }
